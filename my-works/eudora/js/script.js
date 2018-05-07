39cf0b43309e8f040eea1cc37f50e2db9ec7d214
unknown <tranhoangtan1993@mgmail.com>
2018-05-07 10:13:58 +0700

    change JS

--- my-works/eudora/js/script.js
+++ my-works/eudora/js/script.js
@@ -4,31 +4,37 @@ $(document).ready(function() {
 	$('.more').removeClass('clickOn');
 
 	$('.more').click(function(event) {
-		$(this).next().slideToggle(500);
-		$(this).prev().toggleClass('scale');
+		$(this).next().slideDown(500);
+		$(this).prev().addClass('scale');
 		$(this).toggleClass('clickOn');
 	});
 	$('.gallery').slideUp();
 	$('.menu:eq(0)').click(function(event) {
 		event.preventDefault();
+			$('.more').next().slideUp(500);
+			$('.more').prev().removeClass('scale');
 			$('.gallery').slideUp();
 			$('.menu').removeClass('clickOn');
-			$(this).toggleClass('clickOn');
-			$('.gallery:eq(0)').slideToggle();
+			$(this).addClass('clickOn');
+			$('.gallery:eq(0)').slideDown();
 			});
 	$('.menu:eq(1)').click(function(event) {
 		event.preventDefault();
+		$('.more').next().slideUp(500);
+		$('.more').prev().removeClass('scale');
 			$('.gallery').slideUp();
 			$('.menu').removeClass('clickOn');
-			$(this).toggleClass('clickOn');
-			$('.gallery:eq(1)').slideToggle();
+			$(this).addClass('clickOn');
+			$('.gallery:eq(1)').slideDown();
 			});
 	$('.menu:eq(2)').click(function(event) {
 		event.preventDefault();
+		$('.more').next().slideUp(500);
+		$('.more').prev().removeClass('scale');
 			$('.gallery').slideUp();
 			$('.menu').removeClass('clickOn');
-			$(this).toggleClass('clickOn');
-			$('.gallery:eq(2)').slideToggle();
+			$(this).addClass('clickOn');
+			$('.gallery:eq(2)').slideDown();
 			});
 	
 });
